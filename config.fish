fc0945093526bb69adbec2d4713b9a3b061e34aa
Souvik Banerjee <souvik1997@gmail.com>
2016-01-15 18:05:21 -0600

    Change EDITOR and VISUAL

--- config.fish
+++ config.fish
@@ -3,9 +3,15 @@ set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
 set -x PATH ~/.bin/ /usr/local/opt/coreutils/libexec/gnubin $PATH
 set -x MANPATH "/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-set -x EDITOR "/usr/bin/env emacs"
+set -x EDITOR "/usr/bin/env emacsclient -a '' -c"
+set -x VISUAL "/usr/bin/env emacsclient -a '' -c"
 
 function cdt
 	set tempdir (mktemp -d)
 	cd $tempdir
+end
+if [ "$TERM" = "eterm-color" ]
+   function fish_title
+            true
+   end
 end
